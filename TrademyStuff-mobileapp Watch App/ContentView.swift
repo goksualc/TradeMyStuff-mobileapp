f2eeef7406c441c5dd1a594554a78956ca800675
goksualc <goksualcinkaya@gmail.com>
2025-09-02 16:17:23 -0400

    Initial Commit

---
+++ TrademyStuff-mobileapp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrademyStuff-mobileapp Watch App
+//
+//  Created by Göksu Alçınkaya on 9/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrademyStuff-mobileapp Watch App/TrademyStuff_mobileappApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrademyStuff_mobileappApp.swift
+//  TrademyStuff-mobileapp Watch App
+//
+//  Created by Göksu Alçınkaya on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct TrademyStuff_mobileapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
